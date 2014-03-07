f2011f3433ddaf3f0fbeef49087ce350c3a1e5c3
Alexander Pronin <pronin.alx@gmail.com>
2014-03-07 03:53:10 +0200

    Initial Commit

---
+++ 2201TouchesTestHW/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  2201TouchesTestHW
+//
+//  Created by Pronin Alexander on 07.03.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
---
+++ 2201TouchesTestHW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  2201TouchesTestHW
+//
+//  Created by Pronin Alexander on 07.03.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RITAppDelegate class]));
+    }
+}
